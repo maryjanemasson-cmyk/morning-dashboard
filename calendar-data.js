1195c853a68c3de1dee32af576c190a34443353c
Mary Jane Hinrichs <maryjanehinrichs@Marys-MacBook-Pro.local>
2026-05-12 07:00:18 -0400

    Calendar refresh (2026-05-12 07:00)

--- calendar-data.js
+++ calendar-data.js
@@ -12,11 +12,196 @@ window.CALENDAR_EVENTS = [
   }
 ];
 
-window.TRAVEL_DATA = [];
+window.TRAVEL_DATA = [
+  {
+    "destination": "Cambridge, MA",
+    "start": "2026-05-11",
+    "end": "2026-05-15",
+    "icon": "\u2708\ufe0f",
+    "items": [
+      {
+        "summary": "UA408 EWR to BOS",
+        "location": "Newark (EWR)",
+        "description": "",
+        "start": "2026-05-11T10:40:00+00:00",
+        "end": "2026-05-11T11:53:00+00:00",
+        "type": "flight"
+      },
+      {
+        "summary": "Check-in: The Royal Sonesta Boston",
+        "location": "40 Edwin H. Land Boulevard, Cambridge, MA, 02142",
+        "description": "",
+        "start": "2026-05-11T19:00:00+00:00",
+        "end": "2026-05-11T20:00:00+00:00",
+        "type": "hotel"
+      },
+      {
+        "summary": "UA570 BOS to EWR",
+        "location": "Boston (BOS)",
+        "description": "",
+        "start": "2026-05-15T11:00:00+00:00",
+        "end": "2026-05-15T12:29:00+00:00",
+        "type": "flight"
+      },
+      {
+        "summary": "Check-out: The Royal Sonesta Boston",
+        "location": "40 Edwin H. Land Boulevard, Cambridge, MA, 02142",
+        "description": "",
+        "start": "2026-05-15T15:00:00+00:00",
+        "end": "2026-05-15T16:00:00+00:00",
+        "type": "hotel"
+      }
+    ]
+  },
+  {
+    "destination": "Cambridge, MA",
+    "start": "2026-05-19",
+    "end": "2026-05-22",
+    "icon": "\u2708\ufe0f",
+    "items": [
+      {
+        "summary": "UA593 EWR to BOS",
+        "location": "Newark (EWR)",
+        "description": "",
+        "start": "2026-05-19T11:55:00+00:00",
+        "end": "2026-05-19T13:11:00+00:00",
+        "type": "flight"
+      },
+      {
+        "summary": "Check-in: The Royal Sonesta Boston",
+        "location": "40 Edwin H. Land Boulevard, Cambridge, MA, 02142",
+        "description": "",
+        "start": "2026-05-19T19:00:00+00:00",
+        "end": "2026-05-19T20:00:00+00:00",
+        "type": "hotel"
+      },
+      {
+        "summary": "UA570 BOS to EWR",
+        "location": "Boston (BOS)",
+        "description": "",
+        "start": "2026-05-21T11:30:00+00:00",
+        "end": "2026-05-21T12:53:00+00:00",
+        "type": "flight"
+      },
+      {
+        "summary": "Check-out: The Royal Sonesta Boston",
+        "location": "40 Edwin H. Land Boulevard, Cambridge, MA, 02142",
+        "description": "",
+        "start": "2026-05-22T15:00:00+00:00",
+        "end": "2026-05-22T16:00:00+00:00",
+        "type": "hotel"
+      }
+    ]
+  },
+  {
+    "destination": "Chicago, IL",
+    "start": "2026-05-29",
+    "end": "2026-05-29",
+    "icon": "\u2708\ufe0f",
+    "items": [
+      {
+        "summary": "UA1525 EWR to ORD",
+        "location": "Newark (EWR)",
+        "description": "",
+        "start": "2026-05-29T14:24:00+00:00",
+        "end": "2026-05-29T17:06:00+00:00",
+        "type": "flight"
+      }
+    ]
+  },
+  {
+    "destination": "Boston, MA",
+    "start": "2026-06-01",
+    "end": "2026-06-01",
+    "icon": "\u2708\ufe0f",
+    "items": [
+      {
+        "summary": "UA1606 ORD to BOS",
+        "location": "Chicago (ORD)",
+        "description": "",
+        "start": "2026-06-01T15:25:00+00:00",
+        "end": "2026-06-01T18:02:00+00:00",
+        "type": "flight"
+      }
+    ]
+  },
+  {
+    "destination": "Weston, NJ",
+    "start": "2026-06-03",
+    "end": "2026-06-03",
+    "icon": "\u2708\ufe0f",
+    "items": [
+      {
+        "summary": "Amtrak - New London, CT - Union Station (NLC) to Princeton Junction, NJ (PJC)",
+        "location": "New London, CT - Union Station (NLC); NLC, New London, CT - Union Station (NLC)",
+        "description": "",
+        "start": "2026-06-03T23:11:00+00:00",
+        "end": "2026-06-04T03:00:00+00:00",
+        "type": "train"
+      }
+    ]
+  },
+  {
+    "destination": "Lisbon, Portugal",
+    "start": "2026-06-07",
+    "end": "2026-06-12",
+    "icon": "\u2708\ufe0f",
+    "items": [
+      {
+        "summary": "UA64 EWR to LIS",
+        "location": "Newark (EWR)",
+        "description": "",
+        "start": "2026-06-08T00:25:00+00:00",
+        "end": "2026-06-08T07:20:00+00:00",
+        "type": "flight"
+      },
+      {
+        "summary": "UA65 LIS to EWR",
+        "location": "Lisbon (LIS)",
+        "description": "",
+        "start": "2026-06-12T09:25:00+00:00",
+        "end": "2026-06-12T16:55:00+00:00",
+        "type": "flight"
+      }
+    ]
+  },
+  {
+    "destination": "Cambridge, MA",
+    "start": "2026-06-30",
+    "end": "2026-07-03",
+    "icon": "\u2708\ufe0f",
+    "items": [
+      {
+        "summary": "Check-in: Kimpton Marlowe by IHG",
+        "location": "25 Edwin H. Land Blvd, Cambridge, MA, 02141",
+        "description": "",
+        "start": "2026-06-30T19:00:00+00:00",
+        "end": "2026-06-30T20:00:00+00:00",
+        "type": "hotel"
+      },
+      {
+        "summary": "Check-out: Kimpton Marlowe by IHG",
+        "location": "25 Edwin H. Land Blvd, Cambridge, MA, 02141",
+        "description": "",
+        "start": "2026-07-03T15:00:00+00:00",
+        "end": "2026-07-03T16:00:00+00:00",
+        "type": "hotel"
+      }
+    ]
+  }
+];
 
 window.EMAIL_DATA = {
-  "unreadCount": 8,
+  "unreadCount": 7,
   "threads": [
+    {
+      "sender": "Amazon.com",
+      "subject": "Delivery estimate update for your Amazon.com order \u2066#114-8397217-8449052\u2069"
+    },
+    {
+      "sender": "Amazon.com",
+      "subject": "Ordered: \"Gillette Fusion5 Power...\""
+    },
     {
       "sender": "Amazon.com",
       "subject": "Shipped: \u20662\u2069 \"Brillo Steel Wool Soap...\""
@@ -24,18 +209,6 @@ window.EMAIL_DATA = {
     {
       "sender": "WAT THE BRAND",
       "subject": "New Sale Lines Added | As Seen On"
-    },
-    {
-      "sender": "Amazon.com",
-      "subject": "Shipped: \u20662\u2069 \"Zeyune 3 Pcs Spring Green...\" and \u20663\u2069 more items"
-    },
-    {
-      "sender": "PNC Alerts",
-      "subject": "A $364.87 deposit was credited to your account."
-    },
-    {
-      "sender": "FedEx Delivery Manager",
-      "subject": "Your shipment is on the way 871657443460"
     },
     {
       "sender": "NET-A-PORTER Customer Care",
