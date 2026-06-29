93e464d04313a175f90f39965bf0cd26cbd9fcd3
Mary Jane Hinrichs <maryjanemasson@gmail.com>
2026-06-29 15:00:26 -0400

    Calendar refresh (2026-06-29 15:00)

--- calendar-data.js
+++ calendar-data.js
@@ -566,47 +566,43 @@ window.TRAVEL_DATA = [
 ];
 
 window.EMAIL_DATA = {
-  "unreadCount": 21,
+  "unreadCount": 9,
   "threads": [
     {
-      "sender": "Slate Flosser",
-      "subject": "A shipment from order #86347 is on the way"
+      "sender": "PNC Alerts",
+      "subject": "You sent a Zelle\u00ae payment to P & J AUTO BODY & PAINTING LIMITED DBA P"
     },
     {
-      "sender": "DoorDash",
-      "subject": "Details of your no-contact delivery from Mamoun's Falafel"
+      "sender": "Deer Guys",
+      "subject": "Receipt for payment to Deer Guys"
     },
     {
-      "sender": "National Building Museum",
-      "subject": "July at the Museum: THE PLAYGROUND Opens, Summer Late Nights, & More"
+      "sender": "Reformation",
+      "subject": "SHE'S BUSY"
     },
     {
-      "sender": "DoorDash Order",
-      "subject": "Order Confirmation for Mary Jane from Mamoun's Falafel"
+      "sender": "Chan Luu",
+      "subject": "Dressed for Somewhere Warm"
     },
     {
-      "sender": "Etsy",
-      "subject": "Made with heart \ud83e\udde1"
+      "sender": "Amazon.com",
+      "subject": "Shipped: \"Low Back Bra Halter Bra...\" and \u20661\u2069 more item"
     },
     {
-      "sender": "Medium",
-      "subject": "Your free sample of the Medium Newsletter"
-    },
-    {
-      "sender": "BOSS",
-      "subject": "Starting today take up to 50% off"
+      "sender": "IHG One Rewards",
+      "subject": "Enjoy a look at your June IHG One Rewards eStatement"
     },
     {
       "sender": "MyLowe's Rewards",
-      "subject": "FREE beach bucket for members \u2600\ufe0f"
+      "subject": "Pssst! Your next mini bucket\u2019s here \ud83e\udd2b"
+    },
+    {
+      "sender": "Pura",
+      "subject": "Your Office diffuser is out of Velvet Pear"
     },
     {
       "sender": "The New York Times",
-      "subject": "Make the most of your subscription."
-    },
-    {
-      "sender": "Club Pilates Princeton",
-      "subject": "New Booking Confirmation"
+      "subject": "Tell us about your recent experience with Customer Care"
     }
   ],
   "items": [
